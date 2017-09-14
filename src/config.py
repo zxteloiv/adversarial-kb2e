d4ed92fcd79c27b6dece5d1e52c8186f52a5a2d3
Xiang Zhang <zxteloiv@outlook.com>
2017-09-14 14:47:22 +0800

    add config auto init

--- src/config.py
+++ src/config.py
@@ -3,5 +3,18 @@
 import os.path
 
 DATASET_IN_USE = 'FB15k'
-DATAPATH = os.path.join('..', 'data', DATASET_IN_USE)
+DEVICE = -1
+
+# =============================================================================
+# auto computation for data, configurations listed below are generally forbidden
+
+
+def init_conf(dataset):
+    data_path = os.path.join('..', 'data', dataset)
+    train_data = filter(lambda f: 'train' in f, os.listdir(data_path))[0]
+    valid_data = filter(lambda f: 'valid' in f, os.listdir(data_path))[0]
+    test_data = filter(lambda f: 'test' in f, os.listdir(data_path))[0]
+    return data_path, train_data, valid_data, test_data
+
+DATA_PATH, TRAIN_DATA, VALID_DATA, TEST_DATA = init_conf(DATASET_IN_USE)
 
