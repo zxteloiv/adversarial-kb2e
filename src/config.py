212ba60fea4151af22cbb88bdec3d0176f69fb5a
Xiang Zhang <zxteloiv@outlook.com>
2017-09-14 16:24:51 +0800

    add the script to build vocab set for both entities and relations

--- src/config.py
+++ src/config.py
@@ -5,16 +5,30 @@ import os.path
 DATASET_IN_USE = 'FB15k'
 DEVICE = -1
 
+# framework conf
+BATCH_SZ = 100
+
+# model conf
+EMBED_SZ = 50
+
 # =============================================================================
 # auto computation for data, configurations listed below are generally forbidden
 
 
-def init_conf(dataset):
+def init_data(dataset):
     data_path = os.path.join('..', 'data', dataset)
-    train_data = filter(lambda f: 'train' in f, os.listdir(data_path))[0]
-    valid_data = filter(lambda f: 'valid' in f, os.listdir(data_path))[0]
-    test_data = filter(lambda f: 'test' in f, os.listdir(data_path))[0]
+    train_data = os.path.join(data_path, filter(lambda f: 'train' in f, os.listdir(data_path))[0])
+    valid_data = os.path.join(data_path, filter(lambda f: 'valid' in f, os.listdir(data_path))[0])
+    test_data = os.path.join(data_path, filter(lambda f: 'test' in f, os.listdir(data_path))[0])
     return data_path, train_data, valid_data, test_data
 
-DATA_PATH, TRAIN_DATA, VALID_DATA, TEST_DATA = init_conf(DATASET_IN_USE)
+DATA_PATH, TRAIN_DATA, VALID_DATA, TEST_DATA = init_data(DATASET_IN_USE)
+
+def init_vocab(data_path):
+    vocab_ent_filename = os.path.join(data_path, 'vocab_ent.pkl')
+    vocab_rel_filename = os.path.join(data_path, 'vocab_rel.pkl')
+    return vocab_ent_filename, vocab_rel_filename
+
+VOCAB_ENT_FILE, VOCAB_REL_FILE = init_vocab(DATA_PATH)
+
 
--- src/corpus/fb15k.py
+++ src/corpus/fb15k.py
@@ -1,12 +1,31 @@
 # coding: utf-8
 
+from __future__ import absolute_import
 import logging
+from vocabulary import Vocabulary
 
 def reader(generator):
     for l in generator:
         h, r, t = l.rstrip().split('\t')
         yield h, r, t
 
+def build_vocab(filename):
+    vocab_ent, vocab_rel = Vocabulary(), Vocabulary()
+    for h, r, t in reader(open(filename)):
+        vocab_ent.add_token(h)
+        vocab_ent.add_token(t)
+        vocab_rel.add_token(r)
+
+    return vocab_ent, vocab_rel
+
+if __name__ == "__main__":
+    import config
+    import os.path
+    print "build vocab from file %s" % config.TRAIN_DATA
+    vocab_ent, vocab_rel = build_vocab(config.TRAIN_DATA)
+    vocab_ent.save(config.VOCAB_ENT_FILE)
+    vocab_rel.save(config.VOCAB_REL_FILE)
+    print "vocab built successfully at %s and %s" % (config.VOCAB_ENT_FILE, config.VOCAB_REL_FILE)
 
 
 
