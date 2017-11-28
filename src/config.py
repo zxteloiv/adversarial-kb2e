9b705f7cb4cee359ce7180283b8251dc15171d75
Xiang Zhang <zxteloiv@outlook.com>
2017-11-28 19:57:06 +0800

    pretraining TransE for WN18

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@
 import os.path
 
 # environment conf
-DATASET_IN_USE = 'FB15k'
+DATASET_IN_USE = 'WN18'
 DEVICE = 3
 ROOT_PATH = '..'
 
@@ -14,16 +14,16 @@ BATCH_SZ = 1000
 OPT_D_EPOCH = 1  # how many epochs are needed for training a discriminator in one iteration
 OPT_G_EPOCH = 1  # how many epochs are needed for training a generator in one iteration
 SGD_LR = 1e-4  # learning rate for SGD only
-ADAM_ALPHA = 1e-4
+ADAM_ALPHA = 1e-3
 ADAM_BETA1 = 0.9
 SAMPLE_NUM = 10
 
-SAVE_ITER_INTERVAL = 100
+SAVE_ITER_INTERVAL = 1000
 TRAINING_LIMIT = (2000, 'iteration')
 
 # model conf
 EMBED_SZ = 50
-MARGIN = 1.
+MARGIN = 2.
 TRANSE_NORM = 1  # L1 norm =1, L2 norm =2
 
 GRADIENT_CLIP = .1
@@ -31,7 +31,7 @@ WEIGHT_DECAY = .01
 
 TEMPERATURE = .1
 
-DROPOUT = .15
+DROPOUT = .2
 
 DISTANCE_BASED_D = True
 DISTANCE_BASED_G = False
